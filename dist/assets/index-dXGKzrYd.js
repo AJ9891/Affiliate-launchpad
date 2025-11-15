70a0760c467af900de3850e80e4199bc9c5c9b8c
AJ9891 <161670894+AJ9891@users.noreply.github.com>
2025-11-15 08:30:13 +0000

    Add .nojekyll file to public directory to disable Jekyll processing

--- dist/assets/index-dXGKzrYd.js
+++ dist/assets/index-dXGKzrYd.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-CYQQBVu-.js";/*! *****************************************************************************
+import{g as Xu}from"./index-5iCqX7cJ.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
